7bf125a74d6f869a7c6579ea57b04afa6bee68ce
Taylor Radtke <taylorradtke@gmail.com>
2019-10-08 18:03:41 -0400

    added Items query and mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(
+    parent,
+    args,
+    {
+      db: { mutation },
+    },
+    info
+  ) {
+    const item = await mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
+};
 
 module.exports = Mutation;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,17 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db"),
+  // async items(
+  //   parent,
+  //   args,
+  //   {
+  //     db: { query },
+  //   }
+  // ) {
+  //   const items = await query.items();
+  //   return items;
+  // },
+};
 
 module.exports = Query;
