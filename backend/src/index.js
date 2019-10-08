9a1398f0dc0f91f218539bf2a0151808b9f3c27f
Taylor Radtke <taylorradtke@gmail.com>
2019-10-08 09:19:56 -0400

    .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
 
 const app = express();
+const port = process.env.PORT || 4444;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 app.get("/api/greeting", (req, res) => {
-  const name = req.query.name || "World";
+  const { name } = req.query;
+
   res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+  res.send(JSON.stringify({ greeting: `Hello ${name || "World"}!` }));
 });
 
-app.listen(3001, () =>
-  console.log("Express server is running on localhost:3001")
-);
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
