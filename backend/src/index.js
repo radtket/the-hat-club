3fcc6ca82321a632d33651a49694d29d7d88fc00
Taylor Radtke <taylorradtke@gmail.com>
2019-10-08 10:50:41 -0400

    setup graphql backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,16 @@
 require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const pino = require("express-pino-logger")();
+const createServer = require("./createServer");
 
-const app = express();
-const port = process.env.PORT || 4444;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
+const server = createServer();
 
-app.get("/api/greeting", (req, res) => {
-  const { name } = req.query;
-
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ greeting: `Hello ${name || "World"}!` }));
-});
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  ({ port }) => {
+    console.log(`Listening on port ${port}`);
+  }
+);
